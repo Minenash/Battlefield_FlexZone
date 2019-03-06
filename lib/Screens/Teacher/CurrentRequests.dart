3de2b506e1bfb607fe5a03e95a1b4ea042b22875
minenash <minenash@protonmail.com>
2019-03-05 22:23:14 -0500

    Added basic info to the other user type's pages.

--- lib/Screens/Teacher/CurrentRequests.dart
+++ lib/Screens/Teacher/CurrentRequests.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flex_out/User.dart';
 
 class TEA_CurrentRequest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
+      body: Center(
+        child: Text("${User.current.first_inital}. ${User.current.lastname}\n${User.current.email}\n${User.typeToString(User.current.type)}",
+          textAlign: TextAlign.center,
+          textScaleFactor: 1.5,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
