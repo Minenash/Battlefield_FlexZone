a3dffed9d4ab4ee87648586e7be22f782b1706e3
minenash <minenash@protonmail.com>
2019-03-16 00:45:56 -0400

    Moved helper/widget files for Student into a sub-package (for real this time)

---
+++ lib/Screens/Student/widgets/LangaugeSelect.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flex_out/Lang.dart';
+import 'package:flex_out/database.dart';
+
+class LangaugeSelect extends StatefulWidget {
+  
+  State access;
+  
+  LangaugeSelect(this.access, {Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  LangaugeSelectState createState() => LangaugeSelectState(access);
+}
+
+class LangaugeSelectState extends State<LangaugeSelect> {
+
+  String _value = Lang.code;
+  State access;
+  
+  LangaugeSelectState(this.access);
+  
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(Lang.trans('language_dialog_title')),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          _option('en'),
+          _option('es'),
+          _option('de'),
+        ],
+      ),
+      actions: <Widget>[
+        MaterialButton(
+          child: Text(Lang.trans('apply_button')),
+          onPressed: () {
+            setState(() {
+              Navigator.of(context).pop();
+              Future.delayed(Duration(milliseconds: 1), () {
+                Lang.setLang(_value);
+                access.setState((){});
+                Database.saveLangauge();
+              });
+            });
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _option(String code) {
+    return InkWell(
+      onTap: () {setState(() {_value = code;});},
+      child: Row(
+        children: <Widget>[
+          Radio( value: code, groupValue: _value, onChanged: (val) {setState(() {_value = val;});} ),
+          Text( Lang.languages[code] )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Screens/Student/widgets/RequestCardMultiSelect.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flex_out/Screens/Student/CurrentRequests.dart';
+import 'package:flex_out/Request.dart';
+import 'package:flex_out/Screens/Student/widgets/RequestCard_Helper.dart';
+
+class STU_RequestCardMS extends StatefulWidget {
+
+  static Set<Request> selected = new Set();
+
+  Request request;
+
+  STU_RequestCardMS(this.request, {Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  STU_RequestCardMSState createState() => STU_RequestCardMSState(request);
+}
+
+class STU_RequestCardMSState extends State<STU_RequestCardMS> {
+
+  static Map<Request, STU_RequestCardMSState> states = new Map();
+
+  Request r;
+  bool cb_val = false;
+
+  STU_RequestCardMSState(this.r);
+
+  @override
+  void dispose() {
+    states.remove(r);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    states[r] = this;
+
+    Widget baseCard = createBaseCard(r, true);
+
+    return Card(
+        child: Stack(
+          children: <Widget>[
+            baseCard,
+            Container(
+                alignment: Alignment.bottomRight,
+                padding: EdgeInsets.only(top: 10),
+                child: Checkbox(
+                  onChanged: (bool val) { setState(() {
+                    cb_val = val;
+                    if (cb_val)
+                      STU_RequestCardMS.selected.add(r);
+                    else
+                      STU_RequestCardMS.selected.remove(r);
+                    STU_CurrentRequestState.access.setMSAppBarCheckBoxSymbol();
+                  });},
+                  value: cb_val,
+                )
+            )
+          ],
+        )
+    );
+  }
+
+  bool getSelected() {
+    return cb_val;
+  }
+
+  void setSelected(bool val) {
+    setState(() {
+      cb_val = val;
+      if (cb_val)
+        STU_RequestCardMS.selected.add(r);
+      else
+        STU_RequestCardMS.selected.remove(r);
+    });
+  }
+
+  static void setAllSelected(bool val) {
+    for (STU_RequestCardMSState state in states.values)
+      state.setSelected(val);
+  }
+
+}
